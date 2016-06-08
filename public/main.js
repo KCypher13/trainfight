9073619f2ce69a03c770a022b4360be392c2a0b1
Maxime <maxime@reseaulocal.com>
2016-06-08 12:05:59 +0200

    create from to join a room

--- public/main.js
+++ public/main.js
@@ -1,14 +1,22 @@
 var socket = io();
 
 function createRoom(){
-    user.room = $('#room').val();
+    user.room = $('#roomName').val();
     user.pseudo = $('#pseudo').val();
+
 	socket.emit('joinRoom', user.room);
-    socket.emit('changePseudo', user.pseudo)
+    socket.emit('changePseudo', user.pseudo);
+    $('#connexion').addClass('hide');
+    $('#game').removeClass('hide');
+}
+
+function sendAction(){
+    var _station = $(this).data('id');
+    socket.emit('createAction', {'station': _station, 'action': "greveSNCF"});
 }
 
 $(function(){
 
 	$('#createRoom').click(createRoom);
+    $('.station').click(sendAction);
 });
-

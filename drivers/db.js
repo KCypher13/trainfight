57dce4ec50a4c3e45034fce592e0b5e3a73ad52d
Maxime <maxime@reseaulocal.com>
2016-06-09 12:01:56 +0200

    get stationActions when querying stations

--- drivers/db.js
+++ drivers/db.js
@@ -1,30 +1,50 @@
-var mysql   = require('mysql');
-var async   = require('async');
-module.exports = function(app){
+var mysql = require('mysql');
+var async = require('async');
+module.exports = function (app) {
     return {
-        connector : mysql.createConnection(app.config.db),
-        getLines: function(cb){
-            this.connector.query('SELECT * FROM line', function(err, rows, fields){
+        connector: mysql.createConnection(app.config.db),
+        getLines: function (cb) {
+            this.connector.query('SELECT * FROM line', function (err, rows, fields) {
                 if (err) throw err;
-                if(cb){
+                if (cb) {
                     cb(rows);
                 }
             })
         },
-        getStationsFromLines: function(lines, cb){
+        getStationsFromLines: function (lines, cb) {
             var lineWithStations = {};
-            async.forEachOf(lines, function(value, key, callback){
-                lineWithStations[value.id] = {};
-                lineWithStations[value.id].name = value.name;
-                app.db.connector.query('SELECT s.* FROM `line` as l LEFT JOIN `lineStation` AS ls ON l.id = ls.lineId LEFT JOIN `station` AS s ON ls.stationId = s.id WHERE l.id = ?;', value.id, function(err, rows, fields) {
-                    if (err) throw err;
-                    lineWithStations[value.id].stations = rows;
+            async.forEachOf(lines, function (value, key, callback) {
+                    lineWithStations[value.id] = {};
+                    lineWithStations[value.id].name = value.name;
+                    app.db.connector.query('SELECT s.* FROM `line` as l LEFT JOIN `lineStation` AS ls ON l.id = ls.lineId LEFT JOIN `station` AS s ON ls.stationId = s.id WHERE l.id = ?;', value.id, function (err, rows, fields) {
+                        if (err) throw err;
+                        app.db.getActionsFromStations(rows, function(stationsWithActions){
+                            lineWithStations[value.id].stations = stationsWithActions;
+                            callback()
+                        })
+                    });
+                },
+                function (err) {
+                    if (cb) {
+                        cb(lineWithStations)
+                    }
+                });
+        },
+        getActionsFromStations: function (stations, cb) {
+            var stationsWithActions = {};
+            async.forEachOf(stations, function (value, key, callback) {
+                app.db.connector.query('SELECT actionId FROM stationAction WHERE stationId = ?', value.id, function (err, rows, fields) {
+                    var refactedRows = [];
+                    for(key in rows){
+                        refactedRows.push(rows[key].actionId);
+                    }
+                    value.actions = refactedRows;
+                    stationsWithActions[value.id] = value;
                     callback();
                 });
-            },
-            function(err){
-                if(cb){
-                    cb(lineWithStations)
+            }, function (err) {
+                if (cb) {
+                    cb(stationsWithActions);
                 }
             });
         }
--- public/room.js
+++ public/room.js
@@ -6,7 +6,7 @@ var room = {
         this.name = roomName;
         socket.emit('joinRoom', this.name);
         var stateObj = {page: "waitingRoom"};
-        history.pushState(stateObj, "Room" + this.name, "/room/" + this.name);
+        //history.pushState(stateObj, "Room" + this.name, "/room/" + this.name);
     },
     generatePlayerlist: function(){
         $('.manager').text(this.manager);
@@ -18,10 +18,24 @@ var room = {
         var _html = "";
         for(key in lines){
             var _line = lines[key];
-            var _stations = lines[key].stations;
+            var _stations = _line.stations;
             for(key in _stations){
+                ;
                 var _station = _stations[key];
-                _html += '<div id='+_station.name+' class="station mdl-button mdl-js-button mdl-button--icon" data-name="'+_station.name+'" data-line="'+_line.name+'">';
+                _html += '<div id='+_station.name+' class="station mdl-button mdl-js-button mdl-button--icon" data-name="'+_station.name+'" data-line="'+_line.name+'" ';
+                if(_station.actions.length>0){
+                    _html += 'data-actions="';
+                    for(key in _station.actions){
+                        if(key != 0){
+                            _html += ","+_station.actions[key];
+                        }
+                        else{
+                            _html += _station.actions[key];
+                        }
+                    }
+                    _html += '"';
+                }
+                _html += '>';
                 _html += '<div class="markerProgress hide"><p>100%</p></div>';
                 _html += '<p>'+_station.name+'</p>';
                 _html += '</div>';

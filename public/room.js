44469da404d4a8abe7b2aadda98bd59621f6e45d
Maxime <maxime@reseaulocal.com>
2016-06-09 10:54:47 +0200

    refacto variable generateStation

--- public/room.js
+++ public/room.js
@@ -15,19 +15,19 @@ var room = {
         }
     },
     generateStation: function(lines){
-        var html = "";
+        var _html = "";
         for(key in lines){
-            var line = lines[key];
-            var stations = lines[key].stations;
-            for(key in stations){
-                var station = stations[key];
-                html += '<div id='+station.name+' class="station mdl-button mdl-js-button mdl-button--icon" data-name="'+station.name+'" data-line="'+line.name+'">';
-                html += '<div class="markerProgress hide"><p>100%</p></div>';
-                html += '<p>'+station.name+'</p>';
-                html += '</div>';
+            var _line = lines[key];
+            var _stations = lines[key].stations;
+            for(key in _stations){
+                var _station = _stations[key];
+                _html += '<div id='+_station.name+' class="station mdl-button mdl-js-button mdl-button--icon" data-name="'+_station.name+'" data-line="'+_line.name+'">';
+                _html += '<div class="markerProgress hide"><p>100%</p></div>';
+                _html += '<p>'+_station.name+'</p>';
+                _html += '</div>';
             }
         }
-        $('.mapLine').html(html);
+        $('.mapLine').html(_html);
 
     }
 };

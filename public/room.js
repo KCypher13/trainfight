650a103c739125064cab5ae851025d3c364be6c9
Mastère 2 IT <Mastre 2 IT>
2016-06-10 14:19:54 +0200

    Style menu Action and React + fixed stations points

--- public/room.js
+++ public/room.js
@@ -38,7 +38,7 @@ var room = {
                     }
                     _html += '"';
                 }
-                _html += ' style="top:'+(_station.locationY-20)*0.7+'px;left:'+(_station.locationX-15)*0.7+'px">';
+                _html += ' style="top:'+(_station.locationY-23)*0.7+'px;left:'+(_station.locationX-22)*0.7+'px">';
                 _html += '<div class="markerProgress hide"><p>100%</p></div>';
                 _html += '<p class="buttonStation">'+_station.name+'</p>';
                 _html += '</div>';

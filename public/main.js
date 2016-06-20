b224cbf9d215c2ca4233e55d901539d6b9619f46
Maxime <maxime@reseaulocal.com>
2016-06-20 19:23:09 +0200

    debug menu for lower station

--- public/main.js
+++ public/main.js
@@ -116,7 +116,15 @@ function openMenu(){
     var _lineStation = $(this).parent().data('line');
     var _actionId = $(this).parent().data('actions');
     var _actionMenu = $('#actionMenu');
-    var _html = '<div class="customMenu"><div class="triangle"></div><div class="menuContent"><p class="stationName">'+_stationName+'</p><p class="stationLine">'+_lineStation+'</p><ul>';
+    var _html = '<div class="customMenu"><div class="triangle';
+    if($(this).parent().position().top>400){
+        _html += ' reverse';
+    }
+    _html+= '"></div><div class="menuContent';
+    if($(this).parent().position().top>400){
+        _html += ' reverse';
+    }
+    _html += '"><p class="stationName">'+_stationName+'</p><p class="stationLine">'+_lineStation+'</p><ul>';
 
     if(user.role == 'disruptor'){
         _html += generateDisruptorMenu(_actionId, _station);
@@ -127,10 +135,6 @@ function openMenu(){
     _html += "</ul></div></div>";
 
     closeMenu();
-
-    if($(this).parent().position().top>400){
-
-    }
 
     $(this).parent().append(_html);
 }
